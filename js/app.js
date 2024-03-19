3ac77e23dcdd5ab5d46d031990a198a7fc862148
valeriogonini <valeriogonini@gmail.com>
2024-03-19 15:36:04 +0100

    add two list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+
+//SNACK1
+
+
 const membersTable = [
     'Brad Pitt',
     'Johnny Depp',
@@ -26,3 +30,59 @@ return{
 })
 
 console.log(membersTableElement);
+
+
+
+//SNACK2
+
+const studends = [
+    {
+        id : 213,  
+        name : "Marco della Rovere",
+        grades : 78,
+    },
+    {
+        id : 110,  
+        name : "Paola Cortellessa",
+        grades : 96,
+    },
+    {
+        id : 250,  
+        name : "Andrea Mantegna",
+        grades : 48,
+    },
+    {
+        id : 145,  
+        name : "Gaia Mantegna",
+        grades : 74,
+    },
+    {
+        id : 196,  
+        name : "Luigi Grimaldello",
+        grades : 68,
+    },
+    {
+        id : 102,  
+        name : "Piero della Francesca",
+        grades : 50,
+    },
+    {
+        id : 120,  
+        name : "Francesca da Polenta",
+        grades : 84,
+    },
+
+]
+
+//console.log(studend);
+
+
+//Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+let studentsOver70 = studends.filter((element) => element.grades > 70)
+console.log(studentsOver70);
+
+
+//Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+let studentsOver70eId120 = studends.filter((element) => element.grades > 70 && element.id > 120)
+console.log(studentsOver70eId120);
+
